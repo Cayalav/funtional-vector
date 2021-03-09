455678a3ac9e559a8e5f5005c0728a14c2a495db
Carlos Ayala <cayalav17@gmail.com>
2021-03-09 18:44:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# Taller 2- Vectores - Carlos Ayala
+
+#Ejercicio 1
+def num1(n):
+  print("")
+
+#Ejercicio 2
+def num2(n):
+  print("")
+
+#Ejercicio 3
+def num3(n):
+  print("")
+
+#Ejercicio 4
+def num4(n):
+  print("")
+
+#Ejercicio 5
+def num5(n):
+  print("")
+
+#Ejercicio 6
+def num6(n):
+  print("")
+
+#Ejercicio 7
+def num7(n):
+  print("")
+
+
+num1(0)
+num2(0)
+num3(0)
+num4(0)
+num5(0)
+num6(0)
+num7(0)
