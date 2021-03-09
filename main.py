00388b48db8c8e03d5ea0a96082c7a85a965996a
Carlos Ayala <cayalav17@gmail.com>
2021-03-09 19:42:37 +0000

    update(parameters)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 #b. La productoria
 #c. El Mayor Elemento
 #d. El menor Elemento
-def num1(n):
+def num1(v):
   print("")
 
 #Ejercicio 2
@@ -15,30 +15,30 @@ def num1(n):
 #a. Cantidad de elementos pares
 #b. Cantidad de elementos impares
 #c. Cantidad de elementos primos
-def num2(n):
+def num2(v):
   print("")
 
 #Ejercicio 3
-#3. Dado un Vector v y un Vector v1 de cómo resultado 
+#3. Dado un Vector v1 y un Vector v1 de cómo resultado 
 #un Vector resultante de las siguientes operaciones:
 #a. Suma
 #b. Resta
-def num3(n):
+def num3(v1,v2):
   print("")
 
 #Ejercicio 4
 #4. De los n elementos de un vector dado identifique el número que mas se repite e indique cual es.
-def num4(n):
+def num4(v):
   print("")
 
 #Ejercicio 5
 #5. Dado un Vector v de longitud par, divida en 2 partes, en la primera parte realice la productoria y en la segunda la sumatoria. Entregue los valores resultantes.
-def num5(n):
+def num5(v): #Validad si es par
   print("")
 
 #Ejercicio 6
 #6. Dado un vector v, indique si es simétrico, un vector es simétrico si siendo longitud par los números de la primera mitad son iguales al inverso de la otra mitad por ejemplo: X=[1,2,3,3,2,1], en el ejemplo x es un vector simétrico, en caso que la longitud del vector sea impar, se ignorará el elemento central y se seguirá la misma lógica anterior, por ejemplo: Y=[3,5,7,8,7,5,3], en este ejemplo Y es simétrico.
-def num6(n):
+def num6(v):
   print("")
 
 #Ejercicio 7
@@ -47,7 +47,7 @@ def num6(n):
 #b. Intersección: Conjunto que contiene los elementos comunes que aparecen en los conjuntos A y B3
 #c. Diferencia(A-B) Conjunto formado por los elementos que pertenecen al conjunto A y no pertenecen al conjunto B.
 #d. Diferencia (B-A) Conjunto formado por los elementos que pertenecen al conjunto B y no pertenecel al conjunto A. 
-def num7(n):
+def num7(va , vb):
   print("")
 
 
