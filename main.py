6363c87766f14f1f2c572928dec39e8472c76893
Carlos Ayala <cayalav17@gmail.com>
2021-03-09 19:39:49 +0000

    update(enunciados)

--- main.py
+++ main.py
@@ -2,30 +2,51 @@
 # Taller 2- Vectores - Carlos Ayala
 
 #Ejercicio 1
+#1. De los n elementos de un vector dado calcule:
+#a. La sumatoria
+#b. La productoria
+#c. El Mayor Elemento
+#d. El menor Elemento
 def num1(n):
   print("")
 
 #Ejercicio 2
+#2. De los n elementos de un vector dado calcule:
+#a. Cantidad de elementos pares
+#b. Cantidad de elementos impares
+#c. Cantidad de elementos primos
 def num2(n):
   print("")
 
 #Ejercicio 3
+#3. Dado un Vector v y un Vector v1 de cómo resultado 
+#un Vector resultante de las siguientes operaciones:
+#a. Suma
+#b. Resta
 def num3(n):
   print("")
 
 #Ejercicio 4
+#4. De los n elementos de un vector dado identifique el número que mas se repite e indique cual es.
 def num4(n):
   print("")
 
 #Ejercicio 5
+#5. Dado un Vector v de longitud par, divida en 2 partes, en la primera parte realice la productoria y en la segunda la sumatoria. Entregue los valores resultantes.
 def num5(n):
   print("")
 
 #Ejercicio 6
+#6. Dado un vector v, indique si es simétrico, un vector es simétrico si siendo longitud par los números de la primera mitad son iguales al inverso de la otra mitad por ejemplo: X=[1,2,3,3,2,1], en el ejemplo x es un vector simétrico, en caso que la longitud del vector sea impar, se ignorará el elemento central y se seguirá la misma lógica anterior, por ejemplo: Y=[3,5,7,8,7,5,3], en este ejemplo Y es simétrico.
 def num6(n):
   print("")
 
 #Ejercicio 7
+#7. Dado dos vectores númericos A y B debe realizar las siguiente operaciones con conjuntos:
+#a. Unión: Conjunto que contiene(sin repetir) los elementos de A y B.
+#b. Intersección: Conjunto que contiene los elementos comunes que aparecen en los conjuntos A y B3
+#c. Diferencia(A-B) Conjunto formado por los elementos que pertenecen al conjunto A y no pertenecen al conjunto B.
+#d. Diferencia (B-A) Conjunto formado por los elementos que pertenecen al conjunto B y no pertenecel al conjunto A. 
 def num7(n):
   print("")
 
